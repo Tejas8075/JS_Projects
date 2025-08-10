9d78721a173b0bddb34f26b06f21facb1bdc6da1
Tejas8075 <tejasmahangade61@gmail.com>
2025-08-10 22:36:49 +0530

    Completed setup for calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -6,6 +6,29 @@ let isClicked = true;
 
 let flag = 0;
 
+let allClear = document.querySelector(".all-clear");
+let deleteOne = document.querySelector(".delete");
+let percent = document.querySelector(".percent");
+let divide = document.querySelector(".divide");
+let multiply = document.querySelector(".multi");
+let minus = document.querySelector(".minus");
+let plus = document.querySelector(".plus");
+let equalsTo = document.querySelector(".equals");
+
+let seven = document.querySelector(".seven");
+let eight = document.querySelector(".eight");
+let nine = document.querySelector(".nine");
+let four = document.querySelector(".four");
+let five = document.querySelector(".five");
+let six = document.querySelector(".six");
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+let three = document.querySelector(".three");
+let doubleZero = document.querySelector(".double-zero");
+let zero = document.querySelector(".zero");
+let point = document.querySelector(".point");
+
+
 icon.addEventListener("click", () => {
     if (flag == 0) {
         // To Dark mode
